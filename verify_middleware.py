e87db35e8e5610abf39a9a1c2d41826915e1c15d
dmdorta1111 <dmdorta1111@gmail.com>
2026-01-26 23:03:02 -0500

    auto-claude: subtask-3-1 - Create worker metrics module (workers/worker_metrics.py)

--- verify_middleware.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""Verification script for Prometheus middleware."""
-
-import sys
-sys.path.insert(0, 'src')
-
-from pybase.middleware.prometheus_middleware import PrometheusMiddleware
-
-print('OK')
